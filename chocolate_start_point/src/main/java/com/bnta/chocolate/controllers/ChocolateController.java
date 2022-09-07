e25eb64223bf4d38effc3a093ecbdbaeebe7b8b2
Yongran <maiyongran@gmail.com>
2022-09-07 15:03:59 +0100

    add GET

---
+++ chocolate_start_point/src/main/java/com/bnta/chocolate/controllers/ChocolateController.java
@@ -0,0 +1,24 @@
+package com.bnta.chocolate.controllers;
+
+import com.bnta.chocolate.models.Chocolate;
+import com.bnta.chocolate.services.ChocolateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/chocolates")
+public class ChocolateController {
+
+    @Autowired
+    ChocolateService chocolateService;
+
+    @GetMapping
+    public ResponseEntity<List<Chocolate>> getAllChocolates(){
+        List<Chocolate> chocolates = chocolateService.getAllChocolates();
+        return new ResponseEntity<>(chocolates, HttpStatus.OK);
+    }
+}
